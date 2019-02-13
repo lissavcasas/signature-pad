59311e3923ddc75bc52fa06347abb1b2b71e8c02
marimeli <marimel.casas@gmail.com>
2019-02-13 10:41:58 -0500

    get url image

--- main.js
+++ main.js
@@ -1,73 +1,55 @@
 var canvas = document.getElementById('signature-pad');
-var savePng = document.getElementById('save-png');
 var saveJpeg = document.getElementById('save-jpeg');
-var saveSvg = document.getElementById('save-svg');
-var clear = document.getElementById('clear');
-var draw = document.getElementById('draw');
-var erase = document.getElementById('erase');
+var clearButton = document.getElementById('clear');
+var drawButton = document.getElementById('draw');
+var form = document.getElementById('form');
+var image = canvas.toDataURL(); // data:image/png....
 
-// Adjust canvas coordinate space taking into account pixel ratio,
-// to make it look crisp on mobile devices.
-// This also causes canvas to be cleared.
+
 function resizeCanvas() {
-    // When zoomed out to less than 100%, for some very strange reason,
-    // some browsers report devicePixelRatio as less than 1
-    // and only part of the canvas is cleared then.
-    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-    canvas.width = canvas.offsetWidth * ratio;
-    canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext("2d").scale(ratio, ratio);
+  var ratio = Math.max(window.devicePixelRatio || 1, 1);
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
 };
 
 window.onresize = resizeCanvas;
 resizeCanvas();
 
 var signaturePad = new SignaturePad(canvas, {
-  backgroundColor: 'rgb(255, 255, 255)' // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
+  backgroundColor: 'rgb(255, 255, 255)'
 });
 
-savePng.addEventListener('click', function () {
-  if (signaturePad.isEmpty()) {
-    return alert("Please provide a signature first.");
+
+// En el evento submit del formulario, crear una imágen base64 usando toDataURL
+// y colocar ese valor en el textarea del formulario
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  document.getElementById('base64').value = image;
+}, false);
+
+
+// Enviando la imagen usando jQuery AJAX
+$.ajax({
+  url:"service/url/process.php", // no estoy segura de si esta linea esté correcta ?
+  // Enviar un parámetro post con el nombre base64 y con la imagen en el
+  data:{
+      base64: image
+  },
+  // Método POST
+  type:"POST",
+  complete:function(){
+      console.log("Todo en orden");
   }
-  
-  var data = signaturePad.toDataURL('image/png');
-  console.log(data);
-  window.open(data);
 });
 
-saveJpeg.addEventListener('click', function () {
-  if (signaturePad.isEmpty()) {
-    return alert("Please provide a signature first.");
-  }
-
-  var dataUrl = signaturePad.toDataURL('image/jpeg');
-  console.log(dataUrl);
-  window.open(dataUrl);
-});
-
-saveSvg.addEventListener('click', function () {
-  if (signaturePad.isEmpty()) {
-    return alert("Please provide a signature first.");
-  }
-
-  var data = signaturePad.toDataURL('image/svg+xml');
-  console.log(data);
-  console.log(atob(data.split(',')[1]));
-  window.open(data);
-});
-
-clear.addEventListener('click', function () {
+clearButton.addEventListener('click', function () {
   signaturePad.clear();
 });
 
-draw.addEventListener('click', function () {
+drawButton.addEventListener('click', function () {
   var ctx = canvas.getContext('2d');
   console.log(ctx.globalCompositeOperation);
-  ctx.globalCompositeOperation = 'source-over'; // default value
+  ctx.globalCompositeOperation = 'source-over';
 });
 
-erase.addEventListener('click', function () {
-  var ctx = canvas.getContext('2d');
-  ctx.globalCompositeOperation = 'destination-out';
-});
